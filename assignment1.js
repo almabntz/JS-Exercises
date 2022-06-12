55adebc8456abbd0eb79c9068d89a30d23469956
almabntz <106778890+almabntz@users.noreply.github.com>
2022-06-12 10:20:45 -0500

    Add files via upload
    1_VariableExercise

---
+++ assignment1.js
@@ -0,0 +1,124 @@
+// Week 1 - Variables
+
+// In the following exercises, you will need to place your code or answer underneath each
+// exercise prompt.
+
+// First try answering these without using references or looking up any information.
+// Then, check your answer by using references and/or running your code.
+// You can run your JS code using the Chrome or Firefox Developer tools, or by using Node.js.
+// Feel free to update your answers if you got them wrong at first -- this exercise is for your own learning.
+// But make sure you understand why the correct answer is right.
+
+// Data Types and Variables
+
+// Exercise 1. Simply declare a variable named 'emptyVariable'
+
+var 'emptyVariable';  
+
+// Exercise 2. Declare a variable called 'myName'
+var myName; 
+// Exercise 3. Assign your name as the value for 'myName'
+var myName = "Alma"; 
+// Exercise 4. Declare a variable called dreamDestination and give it the value
+// of what your dream vacation destination is.
+var dreamDestination = "Japan";
+// Exercise 5. Make the following variables and assignments
+// - a variable called num1 with a value greater than 1 and less than 10
+// - a variable called num2 with a value of greater than or equal to 1 and less
+// than or equal to 10
+var num1 = n; {n > 1 && n < 10};
+var num2 = m; {m >= 1 && m <= 10};
+
+// Exercise 6. Write a comment below explaining the difference between the rules
+// for num1 and num2 from Exercise 5. What numbers would be valid values for
+// num2 but not num1?
+
+/* num 1 can only be values 2 through 9. It cannot equal 1 or 10, only the numbers between these two values. 
+num2 can equal the value 1 through 10. It includes the constraints*/ 
+
+// Exercise 7. Now we will try some addition.
+// a. Assign the the values of 4 and 6 to num1 and num2, respectively.
+let num1 = 4;
+let num2 = 6;
+// b. Make a new variable called 'theSum', and use 'num1' and
+// 'num2' to assign its value using the "+" operator.
+var theSum = num1 + num2;
+// Exercise 8. Now we will try some multiplication.
+// Make a new variable called 'theProduct', multiply num1 and num2 and assign
+// the resulting value to 'theProduct'.
+var theProduct = num1 * num2;
+// Exercise 9.
+// Make a new variable called 'statement' and using 'myName' and
+// 'dreamDestination' and the concatenation method of your choice, make the
+// value of statement to be:
+// "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
+var statement = "Hi, my name is " + myName +", and I cant wait to visit" + dreamDestination;
+
+// *NOTE* For the following exercises, put your answers in the line below
+// the description of each exercise. For example, below a prompt, you may see:
+// let myAnswerForExercise10= "";
+// If your answer is "foo", then fill in the line to be:
+// let myAnswerForExercise10 = "foo";
+
+// Exercise 10. What is the data type of the value stored in 'dreamDestination',
+// place your answer in the quotes below:
+
+const myAnswerForExercise10 = "string data";
+
+// Exercise 11. What is the data type of the value stored in 'product',
+// place your answer in the quotes below:
+
+const myAnswerForExercise11 = "an integer";
+
+// Exercise 12. What do you think the value of emptyVariable is?
+// Place your answer below:
+
+const myAnswerForExercise12 = "it is undefined because ti doesnt have an assigned value";
+
+// Exercise 13. If
+const A = "R";
+const B = 1;
+const C = 4;
+const D = "D";
+
+// What is the value of B + C
+const myAnswerForExercise13 = "5";
+
+// Exercise 14 What is the value of
+// A + "n" + D
+const myAnswerForExercise14 = "RnD";
+
+// Exercise 15 What is the value of
+// A + B * 2 + D + C / 2:
+const myAnswerForExercise15 = "3";
+
+// Exercise 16 What is the value of A * B
+const myAnswerForExercise16 = "1";
+
+// Exercise 17 What is the value of true || false
+const myAnswerForExercise17 = "both true and false are operators. they are used in boolean logic.";
+
+// Exercise 18 What is the value of 1 === "1"
+const myAnswerForExercise18 = "result will be false because the second value is a string not an integer";
+
+// Exercise 19 What is the value of 1 == "1"
+const myAnswerForExercise19 = "true because the == operator is not as strict as ===";
+
+// Exercise 20 What is the value of 1 < 1
+const myAnswerForExercise20 = "False";
+
+// Exercise 21 What is the value of 1 < 2 < 3
+const myAnswerForExercise21 = "True";
+
+// Exercise 22 What is the value of 2 < 1 < 3
+const myAnswerForExercise22 = "False";
+
+// Congrats, you made it to the end!
+// Did you find this easy or hard? If you used references, which ones helped you?
+// Please answer in a comment below.
+//I was able to retain most of what i learned from freecodebootcamp.com but i did refrence these links.
+// https://devmountain.com/blog/what-are-data-types-javascript-101/#:~:text=In%20Javascript%2C%20there%20are%20five,a%20single%20type%20of%20data.
+// https://www.w3schools.com/jsref/jsref_concat_string.asp
+
+// Email your file to the course staff,
+// or commit your file to GitHub and email us a link.
