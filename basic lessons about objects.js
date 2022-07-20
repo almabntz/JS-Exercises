85c1a718725debae2e4d7c3c47a7722234a9771e
Alma Benitez <106778890+almabntz@users.noreply.github.com>
2022-07-20 16:57:27 -0500

    Add files via upload

---
+++ basic lessons about objects.js
@@ -0,0 +1,82 @@
+//1. Hello Object
+// Write the code line for each action:
+
+//Create an empty object user.
+let user = {};
+console.log(user)
+//Add the property name with the value John.
+    user.name = "John";
+console.log(user)
+//Add the property surname with the value Smith.
+    user.surname = "Smith";
+console.log(user)   
+//Change the value of the name to Pete.
+    user.name = "Pete";
+console.log(user)
+//Remove the property name from the object.
+    delete user.name
+console.log(user)
+
+//2. Check for Emptiness
+//Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+//Should work like that:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false 
+
+function isEmpty(obj){
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+};
+
+//3. Sum object properties
+//We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+//Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+//If salaries is empty, then the result must be 0.
+let sum = 0;
+for (let key in salaries){
+    sum += salaries[key];
+}
+
+//4. Multiply numeric property values by 2
+//Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+//For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+//Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+//P.S. Use typeof to check for a number here.
+
+function multiplyNumeric(obj){
+    for (let key in obj){
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
+    }
+};
